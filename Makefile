7632368885e7e528802341947aca437bfc8dc667
Ray Redondo <rdrpenguin04@gmail.com>
2019-08-01 08:53:06 -0500

    Initial file structure.

---
+++ Makefile
@@ -0,0 +1,12 @@
+CXX := g++
+
+OFILES=src/main.o
+
+LDFLAGS :=
+
+TARGET=wcEmu
+
+all: wcEmu
+
+$(TARGET): $(OFILES) | Makefile
+	$(CXX) -o $(TARGET) $(LDFLAGS) $?
---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+#include <cstdio>
+
+int main() {
+    printf("Warp Chess Emulator v%d.%d\nby Lightning Creations\n\nWarp Chess by (fill in later)\n", 1, 0);
+    return 0;
+}
