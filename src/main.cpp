a093eb63481d08a9ef5fdb43d16949988055e2d2
Ray Redondo <rdrpenguin04@gmail.com>
2019-08-01 16:23:48 -0500

    Start CPU work

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
 #include <cstdio>
 #include "bus.hpp"
+#include "w65c816s.hpp"
 
 int main() {
     printf("Warp Chess Emulator v%d.%d\nby Lightning Creations\n\nWarp Chess by (fill in later)\n", 1, 0);
 
     Bus bus();
+    w65c816s cpu(bus);
 
     return 0;
 }
---
+++ src/w65c816s.hpp
@@ -0,0 +1,7 @@
+class w65c816s {
+public:
+    explicit w65c816s(Bus bus);
+    uint8_t tick(); // returns number of cycles used
+private:
+    Bus bus;
+};
