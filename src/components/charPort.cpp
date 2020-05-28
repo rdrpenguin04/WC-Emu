b324ba727febdcc616128ffd8d762070202c565f
Ray Redondo <rdrpenguin04@gmail.com>
2020-05-28 16:12:21 -0500

    Fix charPort component and add input to wcConnect

--- src/components/charPort.cpp
+++ src/components/charPort.cpp
@@ -10,7 +10,7 @@ bool CharPortComponent::respondToRead(uint32_t loc) {
 
 uint8_t CharPortComponent::read(uint32_t loc) {
   if(loc != 0x4801) {
-    uint8_t *buf = std::malloc(1);
+    uint8_t *buf = (uint8_t*) std::malloc(1);
     recv(sock, buf, 1, 0);
     return buf[0];
   }
--- src/connect.cpp
+++ src/connect.cpp
@@ -1,17 +1,51 @@
 #include "socket.hpp"
 #include <cerrno>
+#include <cstdint>
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
 
-const char *socket_path = "wc_connect_sock";
+// Ready for more platform compatibility heck? Here we go!
+#ifdef _WIN32
+#include <conio.h>
+#else
+// Code from https://cboard.cprogramming.com/c-programming/63166-kbhit-linux.html
+#include <termios.h>
+#include <unistd.h>
+#include <fcntl.h>
 
+int kbhit() {
+    struct termios oldt, newt;
+    int ch;
+    int oldf;
+
+    tcgetattr(STDIN_FILENO, &oldt);
+    newt = oldt;
+    newt.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+    oldf = fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+
+    ch = getchar();
+
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+    fcntl(STDIN_FILENO, F_SETFL, oldf);
+
+    if(ch != EOF) {
+        ungetc(ch, stdin);
+        return 1;
+    }
+
+    return 0;
+}
+#endif
+
+const char *socket_path = "wc_connect_sock";
 
 int main() {
     sockInit();
     std::printf("Warp Chess Connector v1.0\nby Ray Redondo\n");
     int opt = 1;
-    SOCKET sock = socket(AF_UNIX, SOCK_STREAM, 0);
+    SOCKET sock = socket(AF_UNIX, SOCK_STREAM | SOCK_NONBLOCK, 0);
     struct sockaddr_un addr;
     std::memset(&addr, 0, sizeof(addr));
     addr.sun_family = AF_UNIX;
@@ -21,8 +55,13 @@ int main() {
     SOCKET cl = accept(sock, NULL, NULL);
     char *buf = (char*)std::malloc(sizeof(char));
     while(1) {
-        recv(cl, buf, 1, 0);
-        std::printf("%c", buf[0]);
+        while(!kbhit()) {
+            int8_t received = recv(cl, buf, 1, 0);
+            if(received) std::printf("%c", buf[0]);
+        }
+        char c = getchar();
+        putc(c, stdout);
+        sendc(sock, c);
     }
     // sockQuit();
 }
